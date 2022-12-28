d23a5c9b7928a9425eca8d222746ffc4fe9c4395
acwink <acwink@163.com>
2022-12-28 22:22:36 +0800

    fix: type to interface

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,49 +1,44 @@
-import { PlainObjectType, Flatten } from "./shared";
 import { IBannerImage, IRecommendItem } from "./recommend";
 import { IArtist } from "./singers";
 import { IRankItem } from "./rank";
 
-export type ApiRes<T extends PlainObjectType> = {
+interface ApiRes {
   code: string;
-} & T;
+}
 
 // banner 返回值
-export type ApiBanners = Flatten<ApiRes<{ banners: IBannerImage[] }>>;
+export interface ApiBanners extends ApiRes {
+  banners: IBannerImage[];
+}
 
 // 个性化推荐
-export type ApiRecommend = Flatten<
-  ApiRes<{
-    hasTaste: boolean;
-    category: number;
-    result: IRecommendItem[];
-  }>
->;
+export interface ApiRecommend extends ApiRes {
+  hasTaste: boolean;
+  category: number;
+  result: IRecommendItem[];
+}
 
 // 歌手请求
-export type ApiSingerList = Flatten<
-  ApiRes<{
-    more: boolean;
-    artists: IArtist[];
-  }>
->;
+export interface ApiSingerList extends ApiRes {
+  more: boolean;
+  artists: IArtist[];
+}
 
 // 排行榜列表
-export type ApiRankList = Flatten<
-  ApiRes<{
-    list: IRankItem[];
-    artistToplist: {
-      coverUrl: string;
-      artists: Array<{ first: string; second: string; third: number }>;
-      name: string;
-      upateFrequency: string;
-      position: number;
-      updateFrequency: string;
-    };
-    rewardToplist: {
-      coverUrl: string;
-      songs: any[];
-      name: string;
-      position: number;
-    };
-  }>
->;
+export interface ApiRankList extends ApiRes {
+  list: IRankItem[];
+  artistToplist: {
+    coverUrl: string;
+    artists: Array<{ first: string; second: string; third: number }>;
+    name: string;
+    upateFrequency: string;
+    position: number;
+    updateFrequency: string;
+  };
+  rewardToplist: {
+    coverUrl: string;
+    songs: any[];
+    name: string;
+    position: number;
+  };
+}
